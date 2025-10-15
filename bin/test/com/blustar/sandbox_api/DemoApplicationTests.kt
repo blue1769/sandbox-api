79377c230b968acb07d5cd2fbbcb1bb88f524397
여월화 <blue1769@gmail.com>
2025-10-15 17:27:13 +0900

    Initial commit

---
+++ bin/test/com/blustar/sandbox_api/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.blustar.sandbox_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
